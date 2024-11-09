9ea2fc75fa7c963a55be222a9473eefbb629f12f
angge <166150697+anggevictoria@users.noreply.github.com>
2024-11-09 16:02:13 +0800

    Program 1

---
+++ p1.py
@@ -0,0 +1,12 @@
+# Program 1 page 21
+
+print ('hello world')
+print ('What is your name? ')
+myName = input ()
+
+print ('Nice to meet you, ' + myName + '<3')
+print ('the length of your name is ' + str(len(myName)))
+
+print ('How old are you' + myName + '?')
+myAge = input()
+print ('You will be ' + str ((int(myAge) + 1)) + " next year")
